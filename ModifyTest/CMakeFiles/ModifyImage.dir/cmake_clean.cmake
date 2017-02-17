102fde22a377f8e1bd5b3d0ecaf9d906e465ed9a
Chakib Mouzaoui <cmouzaoui@gmail.com>
2017-02-16 19:48:24 -0800

    Initial Github repository setup

---
+++ ModifyTest/CMakeFiles/ModifyImage.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ModifyImage.dir/ModifyTest.cpp.o"
+  "ModifyImage.pdb"
+  "ModifyImage"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ModifyImage.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ VideoDetect/CMakeFiles/VideoDetect.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VideoDetect.dir/VideoDetect.o"
+  "VideoDetect.pdb"
+  "VideoDetect"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VideoDetect.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

75efac090d97325800e2d2edb4dad48066dd96d1
jonathanechang <jonchang331@gmail.com>
2017-05-11 15:55:55 -0700

    Add files via upload

--- ArduinoCode/stepper_demo.ino
+++ ArduinoCode/stepper_demo.ino
@@ -1,21 +1,26 @@
+#include <Servo.h>
+ float angle = -90;
+ 
+ //stepper motor
+ const int M1_Steps = 4;  // pin4 = steps of motor 1
+ const int M1_Dir = 5;    // pin5 = direction of motor 1
+ const int M2_Steps = 6;  // pin6 = steps of motor 2
+ const int M2_Dir = 7;    // pin7 = direction of motor 2
 
-   float angle = -90;
-   const int M1_Dir = 5;
-   const int M1_Steps = 4;
-   const int M2_Dir = 7;
-   const int M2_Steps = 6;
-   float theta0 = 0, phi0 = 0;
-   const int microsteps_per_step = 16;
-   const int time_per_step = 2500/microsteps_per_step;
+//servo(trigger) motor
+Servo trigger;
+const int interruptPin = 2;
+const int servoPin = 10; //yellow signal wire to pin 10
 
-    // pin5 = direction of motor 1
-    // pin4 = steps of motor 1
-    // pin7 = direction of motor 2
-    // pin6 = steps of motor 2
+ float theta0 = 0, phi0 = 0;
+ const int microsteps_per_step = 16;
+ const int time_per_step = 2500/microsteps_per_step;
+
+
 
 void setup() {
   // put your setup code here, to run once:
-  
+     
    pinMode(M1_Dir, OUTPUT);
    pinMode(M1_Steps, OUTPUT);
    pinMode(M2_Dir, OUTPUT);
@@ -24,32 +29,16 @@ void setup() {
    digitalWrite(M1_Steps, LOW);
    digitalWrite(M2_Dir, LOW);
    digitalWrite(M2_Steps, LOW);
+   
+   trigger.attach(servoPin);
+   Serial.begin(9600);
+   trigger.write(20);  //starting position so trigger is vertical
+   delay(500);
+   
    demo();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-//  if (p1 > 0)
-//  {
-//      //motor 1
-//      digitalWrite(M1_Dir, HIGH);      // turn on M1 Direction Pin
-//
-//      //motor 2
-//      //digitalWrite(M2_Dir, HIGH);      // turn on M2 Direction Pin
-//  }
-//
-//	// Steps for motor
-//  for (int i = 0; i < abs(p1); i++)
-//  {
-//      // motor 1
-//      digitalWrite(M1_Steps, HIGH);
-//      //digitalWrite(M2_Steps, HIGH);
-//      delayMicroseconds(time_per_step);      // turn on M1 Steps Pin
-//      digitalWrite(M1_Steps, LOW);
-//      //digitalWrite(M2_Steps, LOW);
-//      delayMicroseconds(time_per_step);       // turn off M1 Steps Pin
-//      //Serial.println("Sent a pulse to both motors.");
-//   }
 
 }
 
@@ -121,18 +110,26 @@ void moveTo(float theta, float phi){
 }
 
 void demo(){
+//  trigger.write(55);
+//  delay(500);
+//  trigger.write(20);
+//  delay(500);
   moveTo(45, 0);
   delay(500);
   moveTo(-90,0);
   delay(500);
   moveTo(45,0);
   delay(500);
-  moveTo(0, 20);
+  moveTo(0, -40);
   delay(500);
-  moveTo(0, -20);
+  moveTo(0, 40);
   delay(500);  
-  moveTo(-45, 30);
+  moveTo(-45,-30);
   delay(500);
-  moveTo(45,-30);
+  moveTo(45, 30);
+  delay(500);
+  trigger.write(55);
+  delay(500);
+  trigger.write(20);
   delay(500);
 }

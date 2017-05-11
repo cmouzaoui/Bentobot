176f77fc96f377299d0a02278d54047f95713bea
comraderayane <cmouzaoui@gmail.com>
2017-05-10 20:45:40 -0700

    cleaned up cmake file

--- CircleDetect/CircleDetect.cpp
+++ CircleDetect/CircleDetect.cpp
@@ -13,57 +13,57 @@ const Mat orangeUpper(1,1,CV_8UC3, Scalar(255,255,255));
 const int veclen = 32;
 int main(int argc, char** argv)
 {
-		if (argc != 2)
-		{
-				printf("usage: DisplayImage <Image_Path>\n");
-				return -1;
-		}
+    if (argc != 2)
+    {
+        printf("usage: DisplayImage <Image_Path>\n");
+        return -1;
+    }
 
-		Mat src, dst, hsv;
-		vector<Point> pts; 
-		src = imread( argv[1], CV_LOAD_IMAGE_COLOR);
+    Mat src, dst, hsv;
+    vector<Point> pts; 
+    src = imread( argv[1], CV_LOAD_IMAGE_COLOR);
 
-		if ( !src.data )
-		{
-				printf("No image data \n");
-				return -1;
-		}
+    if ( !src.data )
+    {
+        printf("No image data \n");
+        return -1;
+    }
 
-		cvtColor(src,hsv, COLOR_BGR2HSV);
-		inRange(hsv, orangeLower, orangeUpper, dst);
-		erode(dst, dst,Mat(),Point(-1,-1), 2, BORDER_CONSTANT, morphologyDefaultBorderValue());
-		dilate(dst, dst,Mat(), Point(-1,-1), 2, BORDER_CONSTANT, morphologyDefaultBorderValue());
+    cvtColor(src,hsv, COLOR_BGR2HSV);
+    inRange(hsv, orangeLower, orangeUpper, dst);
+    erode(dst, dst,Mat(),Point(-1,-1), 2, BORDER_CONSTANT, morphologyDefaultBorderValue());
+    dilate(dst, dst,Mat(), Point(-1,-1), 2, BORDER_CONSTANT, morphologyDefaultBorderValue());
 
-		vector<Mat> cnts;
-		findContours(dst.clone(), cnts, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
-		if (cnts.size() > 0)
-		{
-				for(int i = 0; i < cnts.size(); i++)
-				{
-						Point2f xy;
-						float radius;
-						Mat c = cnts[i];
-						minEnclosingCircle(c, xy, radius);
-						Moments M = moments(c);
-						Point center = cvPoint(int(M.m10/M.m00), int(M.m01/M.m00));
-						if (radius > 10)
-						{
-								circle(src, xy, int(radius), Scalar(0,255,255), 2);
-								circle(src, center, 5, Scalar(0,0,255), -1);
-						}
-						pts.push_back(center);
-				}
-		}
-		for (int i = 0; i < pts.size(); i++)
-		{
-				int thickness = int(sqrt(veclen/ float(i+1)) * 2.5);
-				line(src, pts[veclen-1], pts[veclen-2], Scalar(0,0,255), thickness);
-		}
-		
+    vector<Mat> cnts;
+    findContours(dst.clone(), cnts, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+    if (cnts.size() > 0)
+    {
+        for(int i = 0; i < cnts.size(); i++)
+        {
+            Point2f xy;
+            float radius;
+            Mat c = cnts[i];
+            minEnclosingCircle(c, xy, radius);
+            Moments M = moments(c);
+            Point center = cvPoint(int(M.m10/M.m00), int(M.m01/M.m00));
+            if (radius > 10)
+            {
+                circle(src, xy, int(radius), Scalar(0,255,255), 2);
+                circle(src, center, 5, Scalar(0,0,255), -1);
+            }
+            pts.push_back(center);
+        }
+    }
+    for (int i = 0; i < pts.size(); i++)
+    {
+        int thickness = int(sqrt(veclen/ float(i+1)) * 2.5);
+        line(src, pts[veclen-1], pts[veclen-2], Scalar(0,0,255), thickness);
+    }
 
-		imshow("Display Image", src);
 
-		waitKey(0);
+    imshow("Display Image", src);
 
-		return 0;
+    waitKey(0);
+
+    return 0;
 }
--- VideoDetect/VideoDetect.cpp
+++ VideoDetect/VideoDetect.cpp
@@ -21,12 +21,12 @@ const int threshold_slider_min = 0;
 
 struct Threshold
 {
-	int min[3];
-	int max[3];
+    int min[3];
+    int max[3];
 };
 
 const Threshold orange = {.min = {9,26,228},
-							.max = {42,244,255}};
+    .max = {42,244,255}};
 
 //function primitives
 
@@ -36,118 +36,118 @@ void findCircle(Mat & src, Mat& dst);
 
 int main(int argc, char** argv)
 {
-	// Initialize Videocapture
-	VideoCapture cap0(0);
-	VideoCapture cap1(1);
+    // Initialize Videocapture
+    VideoCapture cap0(0);
+    VideoCapture cap1(1);
 
-	Mat src0, dst0, hsv0;
-	deque<Point> pts0; 
+    Mat src0, dst0, hsv0;
+    deque<Point> pts0; 
 
-	Mat src1, dst1, hsv1; 
-	deque<Point> pts1;
+    Mat src1, dst1, hsv1; 
+    deque<Point> pts1;
 
-	//create instance of threshold
-	Threshold main_thresh = orange;
+    //create instance of threshold
+    Threshold main_thresh = orange;
 
-	//create window for trackbar
-	namedWindow("Threshold", 1);
+    //create window for trackbar
+    namedWindow("Threshold", 1);
 
-	createTrackbar("H min", "Threshold",&main_thresh.min[0],threshold_slider_max);
-	createTrackbar("S min", "Threshold",&main_thresh.min[1],threshold_slider_max);
-	createTrackbar("V min", "Threshold",&main_thresh.min[2],threshold_slider_max);
-	createTrackbar("H max", "Threshold",&main_thresh.max[0],threshold_slider_max);  
-	createTrackbar("S max", "Threshold",&main_thresh.max[1],threshold_slider_max);  
-	createTrackbar("V max", "Threshold",&main_thresh.max[2],threshold_slider_max);  
+    createTrackbar("H min", "Threshold",&main_thresh.min[0],threshold_slider_max);
+    createTrackbar("S min", "Threshold",&main_thresh.min[1],threshold_slider_max);
+    createTrackbar("V min", "Threshold",&main_thresh.min[2],threshold_slider_max);
+    createTrackbar("H max", "Threshold",&main_thresh.max[0],threshold_slider_max);  
+    createTrackbar("S max", "Threshold",&main_thresh.max[1],threshold_slider_max);  
+    createTrackbar("V max", "Threshold",&main_thresh.max[2],threshold_slider_max);  
 
-	//begin loop
-	while(true)
-	{
-	cap0 >> src0;
-	cap1 >> src1;
+    //begin loop
+    while(true)
+    {
+        cap0 >> src0;
+        cap1 >> src1;
 
-//	findInRange(src0,dst0,hsv0,pts0, main_thresh);
-//	findInRange(src1,dst1,hsv1,pts1, main_thresh);
-	findCircle(src0,dst0);
+        //	findInRange(src0,dst0,hsv0,pts0, main_thresh);
+        //	findInRange(src1,dst1,hsv1,pts1, main_thresh);
+        findCircle(src0,dst0);
 
-	imshow("Display Image 0", src0);
-	imshow("Filtered Image 0", dst0);
-//	imshow("Display Image 1", src1);
-//	imshow("Filtered Image 1", dst1);
+        imshow("Display Image 0", src0);
+        imshow("Filtered Image 0", dst0);
+        //	imshow("Display Image 1", src1);
+        //	imshow("Filtered Image 1", dst1);
 
-	//close program when escape key is pressed
-	int key = waitKey(1);
-	if (key == 27) break;
-	}
+        //close program when escape key is pressed
+        int key = waitKey(1);
+        if (key == 27) break;
+    }
 
-	return 0;
+    return 0;
 }
 
 void findInRange(Mat & src,Mat & dst,Mat & hsv, deque<Point> pts, Threshold main_thresh)
 {
-	if ( !src.data )
-	{
-			printf("No image data \n");
-			return;
-	}
+    if ( !src.data )
+    {
+        printf("No image data \n");
+        return;
+    }
 
-	//convert source image to HSV colorspace
-	cvtColor(src,hsv, COLOR_BGR2HSV);
+    //convert source image to HSV colorspace
+    cvtColor(src,hsv, COLOR_BGR2HSV);
 
-	//apply color range
-	inRange(hsv, Scalar(main_thresh.min[0],
-						main_thresh.min[1],
-						main_thresh.min[2]),
-			Scalar(main_thresh.max[0],
-				main_thresh.max[1],
-				main_thresh.max[2]),
-			dst);
+    //apply color range
+    inRange(hsv, Scalar(main_thresh.min[0],
+                main_thresh.min[1],
+                main_thresh.min[2]),
+            Scalar(main_thresh.max[0],
+                main_thresh.max[1],
+                main_thresh.max[2]),
+            dst);
 
-	//erode and dilate for faster processing
-	erode(dst, dst,Mat(),Point(-1,-1), 2, BORDER_CONSTANT, morphologyDefaultBorderValue());
-	dilate(dst, dst,Mat(), Point(-1,-1), 2, BORDER_CONSTANT, morphologyDefaultBorderValue());
+    //erode and dilate for faster processing
+    erode(dst, dst,Mat(),Point(-1,-1), 2, BORDER_CONSTANT, morphologyDefaultBorderValue());
+    dilate(dst, dst,Mat(), Point(-1,-1), 2, BORDER_CONSTANT, morphologyDefaultBorderValue());
 
-	//find contours
-	vector<Mat> cnts;
-	findContours(dst.clone(), cnts, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
-	if (cnts.size() > 0)
-	{
-			//find the contour with max area
-			Mat c = cnts[0];
-			for(int i = 0; i < cnts.size(); i++)
-			{
-					if (contourArea(c) < contourArea(cnts[i]))
-					{
-							c = cnts[i];
-					}
-			}
+    //find contours
+    vector<Mat> cnts;
+    findContours(dst.clone(), cnts, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+    if (cnts.size() > 0)
+    {
+        //find the contour with max area
+        Mat c = cnts[0];
+        for(int i = 0; i < cnts.size(); i++)
+        {
+            if (contourArea(c) < contourArea(cnts[i]))
+            {
+                c = cnts[i];
+            }
+        }
 
-			Point2f xy;
-			float radius;
+        Point2f xy;
+        float radius;
 
-			//find the minimum enclosing circle around that contour
-			minEnclosingCircle(c, xy, radius);
-			//place the center at the centroid of the contour
-			Moments M = moments(c);
-			Point center = cvPoint(int(M.m10/M.m00), int(M.m01/M.m00));
-			//draw the enclosing circle and centroid
-			if (radius > 10)
-			{
-					circle(src, xy, int(radius), Scalar(0,255,255), 2);
-					circle(src, center, 5, Scalar(0,0,255), -1);
-					pts.push_back(center);
-			}
-			if(pts.size() == VECLEN)
-				pts.pop_front();
-	}
-	//draw history trail
-	for (int i = 1; i < pts.size(); i++)
-	{
-			int thickness = int(sqrt(VECLEN/ float(i+1)) * 2.5);
-			if(pts.size()>1)
-			{
-			line(src, pts[i], pts[i-1], Scalar(0,0,255), thickness);
-			}
-	}
+        //find the minimum enclosing circle around that contour
+        minEnclosingCircle(c, xy, radius);
+        //place the center at the centroid of the contour
+        Moments M = moments(c);
+        Point center = cvPoint(int(M.m10/M.m00), int(M.m01/M.m00));
+        //draw the enclosing circle and centroid
+        if (radius > 10)
+        {
+            circle(src, xy, int(radius), Scalar(0,255,255), 2);
+            circle(src, center, 5, Scalar(0,0,255), -1);
+            pts.push_back(center);
+        }
+        if(pts.size() == VECLEN)
+            pts.pop_front();
+    }
+    //draw history trail
+    for (int i = 1; i < pts.size(); i++)
+    {
+        int thickness = int(sqrt(VECLEN/ float(i+1)) * 2.5);
+        if(pts.size()>1)
+        {
+            line(src, pts[i], pts[i-1], Scalar(0,0,255), thickness);
+        }
+    }
 
 }
 
@@ -157,10 +157,10 @@ void findCircle(Mat & src, Mat & gray)
     medianBlur(gray, gray, 5);
     vector<Vec3f> circles;
     HoughCircles(gray, circles, HOUGH_GRADIENT, 1,
-                 gray.rows/16, // change this value to detect circles with different distances to each other
-                 100, 30, 1, 1000 // change the last two parameters
-                                // (min_radius & max_radius) to detect larger circles
-                 );
+            gray.rows/16, // change this value to detect circles with different distances to each other
+            100, 30, 1, 1000 // change the last two parameters
+            // (min_radius & max_radius) to detect larger circles
+            );
     for( size_t i = 0; i < circles.size(); i++ )
     {
         Vec3i c = circles[i];

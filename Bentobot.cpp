ae2cf0c5475183bf94c86218c1dae6f27caec26e
Jill Chang <hchang@Jills-MacBook-Pro.local>
2017-05-21 14:42:31 -0700

    Update coordinates conversion with Jonathan's new equation

--- Bentobot.cpp
+++ Bentobot.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <wiringPi.h>
 using namespace std;
 
 void MotorControl(int p1);
@@ -19,29 +20,39 @@ void MotorControl(int p1)
 	// pin2 = steps of motor 1
 	// pin3 = direction of motor 2
 	// pin4 = steps of motor 2
+    wiringPiSetup();
+    pinMode(1, OUTPUT);
+    pinMode(2, OUTPUT);
+    pinMode(3, OUTPUT);
+    pinMode(4, OUTPUT);
 	bool isP1On = false;
 
-	// motor 1
+	// Directions for motors
 	// assume that pin 1/pin 3 on is cw
-	if (p1 > 0) //to check if the direction is positive
+	if (p1 > 0 && !isP1On)
 	{
 		//turn on pin 1
-		cout << "Turn on pin1" << endl;
-		// turn on pin 3
-		cout << "Turn on pin3" << endl;
-	}
+        digitalWrite(1, HIGH);      // turn on pin1
+        // turn on pin 3
+        digitalWrite(3, HIGH);      // turn on pin3
+        isP1On = true;
+    }
+    else if (p1 < 0 && isP1On)
+    {
+        digitalWrite(1, HIGH);
+        digitalWrite(3, HIGH);
+        isP1On = false;
+    }
 
-	// steps for motor
+	// Steps for motor
 	for (int i = 0; i < abs(p1); i++)
 	{
-		// turn on pin2
-		cout << "Turn on pin2" << endl;
-		// turn off pin2
-		cout << "Turn off pin2" << endl;
-		// turn on pin4
-		cout << "Turn on pin4" << endl;
-		// turn off pin4
-		cout << "Turn off pin4" << endl;
+		// motor 1
+        digitalWrite(2, HIGH); delay(2.5);      // turn on pin2
+        digitalWrite(2, LOW); delay(2.5);       // turn off pin2
+		// motor 2
+        digitalWrite(4, HIGH); delay(2.5);      // turn on pin4
+        digitalWrite(4, LOW); delay(2.5);       // turn off pin4
 	}
 
 }
--- SC/SC.cpp
+++ SC/SC.cpp
@@ -28,13 +28,13 @@ int main()
 
 void cartToPolar(float x, float y, float x, float& phi, float& theta)
 {
-	float r1 = 0, r2 = 0, r3, phi_o = 0, a, b;
+	float r1 = 218.7, r2 = 24.5, r3, phi_o = 48.76, a, b;
 	
-	theta = atan(y/x);
+	theta = -atan(y/x);
 	r3 = sqrt(x^2 + y^2 + (z-r1)^2 -r2^2);
 	a = atan(r2/r3);
 	b = atan((z-r1)/(srqt(x^2 + y^2));
-	phi = b - a + phi_o;
+	phi = b - a;
 
 	cout << "Phi: " << phi << endl;
 	cout << "Theta: " << theta << endl;

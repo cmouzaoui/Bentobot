3a1bd611bbd3f37b91bb52a404c8ab5773863554
Jonathan Chang <jechang331@ucla.edu>
2017-05-23 11:14:28 -0700

    Deleted Stepper,hello, and stepper_demo files. Added Static code.

--- ArduinoCode/Static_Arduino/Static_Arduino.ino
+++ ArduinoCode/Static_Arduino/Static_Arduino.ino
@@ -1,22 +1,22 @@
 #include <Servo.h>
- float angle = -90;
- 
- //stepper motor
- const int M1_Steps = 4;  // pin4 = steps of motor 1
- const int M1_Dir = 5;    // pin5 = direction of motor 1
- const int M2_Steps = 6;  // pin6 = steps of motor 2
- const int M2_Dir = 7;    // pin7 = direction of motor 2
+  
+//stepper motor
+const int M1_Steps = 4;  // pin4 = steps of motor 1
+const int M1_Dir = 5;    // pin5 = direction of motor 1
+const int M2_Steps = 6;  // pin6 = steps of motor 2
+const int M2_Dir = 7;    // pin7 = direction of motor 2
 
 //servo(trigger) motor
 Servo trigger;
-const int interruptPin = 2;
 const int servoPin = 10; //yellow signal wire to pin 10
 
- float theta0 = 0, phi0 = 0;
- const int microsteps_per_step = 16;
- const int time_per_step = 2500/microsteps_per_step;
-
-
+float theta0 = 0, phi0 = 48.76;  //48.76 is when launcher is resting backwards
+float theta_now = theta0;
+float phi_now = phi0;
+const int microsteps_per_step = 16;
+const int time_per_step = 2500/microsteps_per_step;
+ 
+ 
 
 void setup() {
   // put your setup code here, to run once:
@@ -31,15 +31,27 @@ void setup() {
    digitalWrite(M2_Steps, LOW);
    
    trigger.attach(servoPin);
+    
    Serial.begin(9600);
    trigger.write(20);  //starting position so trigger is vertical
    delay(500);
-   
-   demo();
+//   Serial.println("setup");
+//   demo();
 }
 
 void loop() {
 
+  //look for the next valid integer in the incoming serial stream
+  while (Serial.available()>0){
+    float theta = Serial.parseFloat();  //theta value
+    float phi = Serial.parseFloat();  //phi value
+
+    //look for the newline
+    if (Serial.read() == '\n') {
+      moveTo(theta,phi);
+      Serial.print(theta); Serial.print(phi);      
+    }
+  }
 }
 
 void move(float p1, float p2){
@@ -106,29 +118,32 @@ void move(float p1, float p2){
 }
 
 void moveTo(float theta, float phi){
-  move(theta-theta0, phi-phi0);
+  move(theta-theta_now, phi-phi_now);
+//  Serial.print("We want to move this much: "); Serial.print(theta-theta_now); Serial.print(",");  Serial.println(phi-phi_now);
+//  Serial.print("We want to move this much: "); Serial.print(theta-theta0); Serial.print(",");  Serial.println(phi-phi_now);
+  theta_now = theta;
+  phi_now = phi;
+  
+//  Serial.print("The new position is: "); Serial.print(theta_now); Serial.print(","); Serial.println(phi_now);
 }
 
 void demo(){
-//  trigger.write(55);
-//  delay(500);
-//  trigger.write(20);
-//  delay(500);
+
   moveTo(45, 0);
   delay(500);
-  moveTo(-90,0);
+  moveTo(-45,0);
   delay(500);
   moveTo(45,0);
   delay(500);
-  moveTo(0, -40);
+  moveTo(0, 10);
   delay(500);
-  moveTo(0, 40);
+  moveTo(0, 45);
   delay(500);  
-  moveTo(-45,-30);
+  moveTo(-45,30);
   delay(500);
-  moveTo(45, 30);
+  moveTo(0, 45);
   delay(500);
-  trigger.write(55);
+  trigger.write(60);
   delay(500);
   trigger.write(20);
   delay(500);
